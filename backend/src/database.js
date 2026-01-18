7c6ea2fe068c60cb42ec3d7d89092680f22a515f
Bhargavi Aakrapu <bbhargavi@gmail.com>
2026-01-18 19:37:01 +0530

    Fixed DB connection for Render free plan

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,11 +1,14 @@
 const { Sequelize } = require("sequelize");
-require("dotenv").config({
-  path: process.env.NODE_ENV === "production" ? ".env.production" : ".env",
-});
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 module.exports = sequelize;
