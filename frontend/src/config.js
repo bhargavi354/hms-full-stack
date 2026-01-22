8cb387914aa27b9bcc81244d89e20d749390e4aa
bhargavi354 <bbhargavi738@gmail.com>
2026-01-22 19:23:25 +0530

    Update API_BASE to production URL

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,5 @@
 // frontend/src/config.js
-const API_BASE =
-  process.env.REACT_APP_API_BASE ||
-  "http://localhost:4000";
+
+const API_BASE = "https://hms-backend-ahsd.onrender.com/api";
 
 export default API_BASE;
