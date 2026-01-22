2d92aa740f49b9efcd7a2317aa9b05be3f09224b
bhargavi354 <bbhargavi738@gmail.com>
2026-01-22 17:55:18 +0530

    Refactor database connection and model imports

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,12 +1,6 @@
 const { Sequelize, DataTypes } = require("sequelize");
 
-const databaseUrl = process.env.DATABASE_URL;
-
-if (!databaseUrl) {
-  throw new Error("DATABASE_URL is not set");
-}
-
-const sequelize = new Sequelize(databaseUrl, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   protocol: "postgres",
   logging: false,
@@ -18,22 +12,22 @@ const sequelize = new Sequelize(databaseUrl, {
   },
 });
 
-// Import models
+// Load models
 const Admin = require("./admin")(sequelize, DataTypes);
-const Patient = require("./patient")(sequelize, DataTypes);
 const Employee = require("./employee")(sequelize, DataTypes);
+const Patient = require("./patient")(sequelize, DataTypes);
 const Attendance = require("./attendance")(sequelize, DataTypes);
 const Revenue = require("./revenue")(sequelize, DataTypes);
 const OpBooking = require("./OpBooking")(sequelize, DataTypes);
 const OpInvoice = require("./OpInvoice")(sequelize, DataTypes);
 const Settings = require("./setting")(sequelize, DataTypes);
 
-// Export everything
+// Export all
 module.exports = {
   sequelize,
   Admin,
-  Patient,
   Employee,
+  Patient,
   Attendance,
   Revenue,
   OpBooking,
