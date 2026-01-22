6999317b0e399681ce3f68c85be4daadaf024d86
bhargavi354 <bbhargavi738@gmail.com>
2026-01-22 20:10:24 +0530

    Use environment variable for API_BASE configuration

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
-// frontend/src/config.js
-
-const API_BASE = "https://hms-backend-ahsd.onrender.com/api";
+const API_BASE =
+  process.env.REACT_APP_API_BASE ||
+  "https://hms-backend-ahsd.onrender.com/api";
 
 export default API_BASE;
