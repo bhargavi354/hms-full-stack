5a76d6cd8371edce9c6d3b7fbb744992c8ff48ad
bhargavi354 <bbhargavi738@gmail.com>
2026-01-22 14:06:34 +0530

    Import createAdmin module in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const { sequelize } = require("./models");
+require("./createAdmin");
 
 const app = express();
 
