ea7b0f43787025b62d5dcdf0a6b142a411293329
bhargavi354 <bbhargavi738@gmail.com>
2026-01-22 17:18:40 +0530

    Format API_BASE assignment for readability

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 // frontend/src/config.js
-
 const API_BASE =
-  process.env.REACT_APP_API_BASE || "http://localhost:4000";
+  process.env.REACT_APP_API_BASE ||
+  "http://localhost:4000";
 
 export default API_BASE;
